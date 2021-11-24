307ecb3af0fe81628ec3ac13f17d1746a11233b8
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-11-24 06:35:12 +0300

    Add images

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,6 +14,8 @@ export default Section
 const Wrap = styles.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
-
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
 `
