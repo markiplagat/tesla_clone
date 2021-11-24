36c05d5696a37d03c54cc2464eeff8519087a8d9
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-11-24 10:24:14 +0300

    Style the common section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,65 @@
 import React from 'react'
-import styles from 'styled-components'
+import styled from 'styled-components'
 
 function Section() {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order online for touches delivery</p>
+            </ItemText>
+
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
 
 export default Section
 
-const Wrap = styles.div`
+const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+`
+
+const RightButton = styled(LeftButton)`
+    
 `
