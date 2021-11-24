468196b400113af503072823eb0d6fde6920ee39
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-11-24 06:32:03 +0300

    Add section for common data

---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from 'styled-components'
+
+function Section() {
+    return (
+        <Wrap>
+            
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styles.div`
+    width: 100vw;
+    height: 100vh;
+    background: orange;
+
+`
