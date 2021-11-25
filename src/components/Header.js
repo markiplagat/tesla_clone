3256b160c313b85ac11c6bbc268ff3462d2b6a7a
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-11-25 14:19:40 +0300

    Add Header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="tesla" />
+            </a>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+                <p><a href="#">Model Y</a></p>
+            </Menu>
+        </Container>
     )
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+    
+    p {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+`
